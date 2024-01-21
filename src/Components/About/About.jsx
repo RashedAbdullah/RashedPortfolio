71c01dac3d220bcdfa0a338644f8c16f1e4f421f
Rashed Abdullah <maarashed40@gmail.com>
2024-01-21 10:11:14 +0600

    more functionalities

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import './About.css';
-import { useTranslation } from 'react-i18next';
+import "./About.css";
+import { useTranslation } from "react-i18next";
 
 const About = () => {
-
   const { t } = useTranslation();
   return (
-    <div className='mainAboutDiv'>
-      <div className='aboutBox'>
+    <div className="mainAboutDiv">
+      <div className="aboutBox">
         <h2>{t("aboutMe")}</h2>
-        <p>{t("underAbout")}  <span>{t("js")}</span> {t("and")} <span>{t("react")}</span></p>
+        <p>
+          {t("underAbout")} <span>{t("js")}</span> {t("and")}{" "}
+          <span>{t("react")}</span>
+        </p>
 
         <h3>{t("whoIAm")}</h3>
         <p>{t("underWhoIam")}</p>
-        <div><button>{t("knowMoreBtn")}</button></div>
+        <div>
+          <button>{t("knowMoreBtn")}</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
